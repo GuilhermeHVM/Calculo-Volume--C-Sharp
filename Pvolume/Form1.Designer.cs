2fecab8f3e66dd4ea979c5cefbf3d48fed686467
GuilhermeHVM <115192384+GuilhermeHVM@users.noreply.github.com>
2022-10-08 15:12:20 -0300

    Add files via upload

---
+++ Pvolume/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace Pvolume
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblRaio = new System.Windows.Forms.Label();
+            this.lblAltura = new System.Windows.Forms.Label();
+            this.lblVolume = new System.Windows.Forms.Label();
+            this.txtRaio = new System.Windows.Forms.TextBox();
+            this.txtAltura = new System.Windows.Forms.TextBox();
+            this.txtVolume = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(79, 326);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(75, 23);
+            this.btnCalcular.TabIndex = 0;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(327, 326);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(75, 23);
+            this.btnLimpar.TabIndex = 1;
+            this.btnLimpar.Text = "Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(552, 326);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(75, 23);
+            this.btnSair.TabIndex = 2;
+            this.btnSair.Text = "Sair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lblRaio
+            // 
+            this.lblRaio.AutoSize = true;
+            this.lblRaio.Location = new System.Drawing.Point(52, 59);
+            this.lblRaio.Name = "lblRaio";
+            this.lblRaio.Size = new System.Drawing.Size(29, 13);
+            this.lblRaio.TabIndex = 3;
+            this.lblRaio.Text = "Raio";
+            // 
+            // lblAltura
+            // 
+            this.lblAltura.AutoSize = true;
+            this.lblAltura.Location = new System.Drawing.Point(52, 133);
+            this.lblAltura.Name = "lblAltura";
+            this.lblAltura.Size = new System.Drawing.Size(34, 13);
+            this.lblAltura.TabIndex = 4;
+            this.lblAltura.Text = "Altura";
+            // 
+            // lblVolume
+            // 
+            this.lblVolume.AutoSize = true;
+            this.lblVolume.Location = new System.Drawing.Point(52, 217);
+            this.lblVolume.Name = "lblVolume";
+            this.lblVolume.Size = new System.Drawing.Size(42, 13);
+            this.lblVolume.TabIndex = 5;
+            this.lblVolume.Text = "Volume";
+            // 
+            // txtRaio
+            // 
+            this.txtRaio.Location = new System.Drawing.Point(131, 59);
+            this.txtRaio.Name = "txtRaio";
+            this.txtRaio.Size = new System.Drawing.Size(175, 20);
+            this.txtRaio.TabIndex = 6;
+            // 
+            // txtAltura
+            // 
+            this.txtAltura.Location = new System.Drawing.Point(131, 133);
+            this.txtAltura.Name = "txtAltura";
+            this.txtAltura.Size = new System.Drawing.Size(175, 20);
+            this.txtAltura.TabIndex = 7;
+            // 
+            // txtVolume
+            // 
+            this.txtVolume.Location = new System.Drawing.Point(131, 217);
+            this.txtVolume.Name = "txtVolume";
+            this.txtVolume.ReadOnly = true;
+            this.txtVolume.Size = new System.Drawing.Size(175, 20);
+            this.txtVolume.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(808, 407);
+            this.Controls.Add(this.txtVolume);
+            this.Controls.Add(this.txtAltura);
+            this.Controls.Add(this.txtRaio);
+            this.Controls.Add(this.lblVolume);
+            this.Controls.Add(this.lblAltura);
+            this.Controls.Add(this.lblRaio);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblRaio;
+        private System.Windows.Forms.Label lblAltura;
+        private System.Windows.Forms.Label lblVolume;
+        private System.Windows.Forms.TextBox txtRaio;
+        private System.Windows.Forms.TextBox txtAltura;
+        private System.Windows.Forms.TextBox txtVolume;
+    }
+}
+
